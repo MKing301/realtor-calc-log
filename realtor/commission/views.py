e82692b568108641a785d23af6134aa89d40e447
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-08-31 21:38:54 -0400

    Add calculator view

--- realtor/commission/views.py
+++ realtor/commission/views.py
@@ -5,3 +5,8 @@ def index(request):
     return render(request=request,
                   template_name="commission/index.html"
                 )
+
+def calculator(request):
+    return render(request=request,
+                  template_name="commission/calculator.html"
+                )
